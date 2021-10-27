a292b33f716c004117b6ad4297c6fdf034a89bcd
Dan <33081566+deobfuscate@users.noreply.github.com>
2021-10-26 22:37:26 -0700

    PHP Settings dialog

---
+++ wampmon/frmPHPSettings.Designer.cs
@@ -0,0 +1,329 @@
+﻿namespace wampmon
+{
+    partial class frmPHPSettings
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmPHPSettings));
+            this.pnlTitlebar = new System.Windows.Forms.Panel();
+            this.btnMinimize = new System.Windows.Forms.Button();
+            this.btnCloseWindow = new System.Windows.Forms.Button();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtServerName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtServerAdmin = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtLogLevel = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtDocRoot = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtErrorLog = new System.Windows.Forms.TextBox();
+            this.pnlTitlebar.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlTitlebar
+            // 
+            this.pnlTitlebar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(10)))), ((int)(((byte)(10)))));
+            this.pnlTitlebar.Controls.Add(this.btnMinimize);
+            this.pnlTitlebar.Controls.Add(this.btnCloseWindow);
+            this.pnlTitlebar.Controls.Add(this.lblTitle);
+            this.pnlTitlebar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlTitlebar.Location = new System.Drawing.Point(0, 0);
+            this.pnlTitlebar.Name = "pnlTitlebar";
+            this.pnlTitlebar.Size = new System.Drawing.Size(324, 31);
+            this.pnlTitlebar.TabIndex = 2;
+            this.pnlTitlebar.DoubleClick += new System.EventHandler(this.TitleDoubleClick);
+            this.pnlTitlebar.MouseMove += new System.Windows.Forms.MouseEventHandler(this.TitleMouseMove);
+            // 
+            // btnMinimize
+            // 
+            this.btnMinimize.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(10)))), ((int)(((byte)(10)))));
+            this.btnMinimize.FlatAppearance.BorderSize = 0;
+            this.btnMinimize.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(75)))), ((int)(((byte)(75)))), ((int)(((byte)(75)))));
+            this.btnMinimize.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.btnMinimize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMinimize.ForeColor = System.Drawing.Color.White;
+            this.btnMinimize.Location = new System.Drawing.Point(472, 0);
+            this.btnMinimize.Name = "btnMinimize";
+            this.btnMinimize.Size = new System.Drawing.Size(45, 31);
+            this.btnMinimize.TabIndex = 3;
+            this.btnMinimize.Text = "＿";
+            this.btnMinimize.UseVisualStyleBackColor = false;
+            this.btnMinimize.Click += new System.EventHandler(this.btnMinimize_Click);
+            // 
+            // btnCloseWindow
+            // 
+            this.btnCloseWindow.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(10)))), ((int)(((byte)(10)))));
+            this.btnCloseWindow.FlatAppearance.BorderSize = 0;
+            this.btnCloseWindow.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(75)))), ((int)(((byte)(75)))), ((int)(((byte)(75)))));
+            this.btnCloseWindow.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.btnCloseWindow.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCloseWindow.ForeColor = System.Drawing.Color.White;
+            this.btnCloseWindow.Location = new System.Drawing.Point(517, 0);
+            this.btnCloseWindow.Name = "btnCloseWindow";
+            this.btnCloseWindow.Size = new System.Drawing.Size(45, 31);
+            this.btnCloseWindow.TabIndex = 2;
+            this.btnCloseWindow.Text = "✕";
+            this.btnCloseWindow.UseVisualStyleBackColor = false;
+            this.btnCloseWindow.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.Color.White;
+            this.lblTitle.Location = new System.Drawing.Point(8, 6);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(223, 20);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "WAMP Monitor - PHP Settings";
+            this.lblTitle.MouseMove += new System.Windows.Forms.MouseEventHandler(this.TitleLabelMouseMove);
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.btnSave.FlatAppearance.BorderSize = 0;
+            this.btnSave.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(75)))), ((int)(((byte)(75)))), ((int)(((byte)(75)))));
+            this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSave.ForeColor = System.Drawing.Color.White;
+            this.btnSave.Location = new System.Drawing.Point(116, 213);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(94, 36);
+            this.btnSave.TabIndex = 4;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = false;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(75)))), ((int)(((byte)(75)))), ((int)(((byte)(75)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(216, 213);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 36);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Close";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.btnCloseWindow_Click);
+            // 
+            // txtPort
+            // 
+            this.txtPort.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.txtPort.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtPort.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPort.ForeColor = System.Drawing.Color.White;
+            this.txtPort.Location = new System.Drawing.Point(103, 123);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtPort.Size = new System.Drawing.Size(61, 22);
+            this.txtPort.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(68, 127);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Port:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(25, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Server Name:";
+            // 
+            // txtServerName
+            // 
+            this.txtServerName.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.txtServerName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtServerName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtServerName.ForeColor = System.Drawing.Color.White;
+            this.txtServerName.Location = new System.Drawing.Point(103, 67);
+            this.txtServerName.Name = "txtServerName";
+            this.txtServerName.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtServerName.Size = new System.Drawing.Size(207, 22);
+            this.txtServerName.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(24, 99);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Server Admin:";
+            // 
+            // txtServerAdmin
+            // 
+            this.txtServerAdmin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.txtServerAdmin.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtServerAdmin.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtServerAdmin.ForeColor = System.Drawing.Color.White;
+            this.txtServerAdmin.Location = new System.Drawing.Point(103, 95);
+            this.txtServerAdmin.Name = "txtServerAdmin";
+            this.txtServerAdmin.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtServerAdmin.Size = new System.Drawing.Size(207, 22);
+            this.txtServerAdmin.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(40, 181);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Log Level:";
+            // 
+            // txtLogLevel
+            // 
+            this.txtLogLevel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.txtLogLevel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtLogLevel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLogLevel.ForeColor = System.Drawing.Color.White;
+            this.txtLogLevel.Location = new System.Drawing.Point(103, 177);
+            this.txtLogLevel.Name = "txtLogLevel";
+            this.txtLogLevel.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtLogLevel.Size = new System.Drawing.Size(207, 22);
+            this.txtLogLevel.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(12, 43);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(85, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Document Root:";
+            // 
+            // txtDocRoot
+            // 
+            this.txtDocRoot.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.txtDocRoot.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtDocRoot.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDocRoot.ForeColor = System.Drawing.Color.White;
+            this.txtDocRoot.Location = new System.Drawing.Point(103, 39);
+            this.txtDocRoot.Name = "txtDocRoot";
+            this.txtDocRoot.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtDocRoot.Size = new System.Drawing.Size(207, 22);
+            this.txtDocRoot.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(44, 154);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Error Log:";
+            // 
+            // txtErrorLog
+            // 
+            this.txtErrorLog.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.txtErrorLog.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtErrorLog.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtErrorLog.ForeColor = System.Drawing.Color.White;
+            this.txtErrorLog.Location = new System.Drawing.Point(103, 149);
+            this.txtErrorLog.Name = "txtErrorLog";
+            this.txtErrorLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtErrorLog.Size = new System.Drawing.Size(207, 22);
+            this.txtErrorLog.TabIndex = 14;
+            // 
+            // frmPHPSettings
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(18)))), ((int)(((byte)(18)))));
+            this.ClientSize = new System.Drawing.Size(324, 261);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtErrorLog);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtDocRoot);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtLogLevel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtServerAdmin);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtServerName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.pnlTitlebar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(300, 250);
+            this.Name = "frmPHPSettings";
+            this.Text = "WAMP Monitor - Access logs";
+            this.Load += new System.EventHandler(this.frmLogs_Load);
+            this.Resize += new System.EventHandler(this.ResizeLogWindow);
+            this.pnlTitlebar.ResumeLayout(false);
+            this.pnlTitlebar.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlTitlebar;
+        private System.Windows.Forms.Button btnMinimize;
+        private System.Windows.Forms.Button btnCloseWindow;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtServerName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtServerAdmin;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtLogLevel;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtDocRoot;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtErrorLog;
+    }
+}
---
+++ wampmon/frmPHPSettings.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace wampmon
+{
+    public partial class frmPHPSettings : Form
+    {
+        private const int WM_NCLBUTTONDOWN = 0xA1;
+        private const int HT_CAPTION = 0x2;
+        private const int GRIP_SIZE = 14;
+
+        [DllImport("user32.dll")]
+        private static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [DllImport("user32.dll")]
+        private static extern bool ReleaseCapture();
+        private string configFile;
+        
+        public frmPHPSettings(string configFile)
+        {
+            InitializeComponent();
+            DoubleBuffered = true;
+            //SetStyle(ControlStyles.ResizeRedraw, true);
+            this.configFile = configFile;
+        }
+
+        private string ReadSetting(string filename, string setting)
+        {
+            string line;
+            StreamReader file = new StreamReader(filename);
+            while ((line = file.ReadLine()) != null)
+            {
+                if (line == "" || line[0] == '#') continue;
+                var split = line.Split(' ');
+                if (split[0] == setting)
+                    return split[1];
+            }
+            return null;
+        }
+
+        private void TitleDoubleClick(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Normal) WindowState = FormWindowState.Maximized;
+            else WindowState = FormWindowState.Normal;
+        }
+
+        private void TitleMouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        /*protected override void OnPaint(PaintEventArgs e)
+        {
+            Rectangle rc = new Rectangle(ClientSize.Width - GRIP_SIZE, ClientSize.Height - GRIP_SIZE, GRIP_SIZE, GRIP_SIZE);
+            ControlPaint.DrawSizeGrip(e.Graphics, BackColor, rc);
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == 0x84)
+            {  // Trap WM_NCHITTEST
+                Point pos = new Point(m.LParam.ToInt32());
+                pos = PointToClient(pos);
+
+                if (pos.X >= ClientSize.Width - GRIP_SIZE && pos.Y >= ClientSize.Height - GRIP_SIZE)
+                {
+                    m.Result = (IntPtr)17; // HTBOTTOMRIGHT
+                    return;
+                }
+            }
+            base.WndProc(ref m);
+        }*/
+
+        private void TitleLabelMouseMove(object sender, MouseEventArgs e)
+        {
+            TitleMouseMove(sender, e);
+        }
+
+        private void btnMinimize_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void frmLogs_Load(object sender, EventArgs e)
+        {
+            txtDocRoot.Text = ReadSetting(configFile, "DocumentRoot");
+            txtServerName.Text = ReadSetting(configFile, "ServerName");
+            txtServerAdmin.Text = ReadSetting(configFile, "ServerAdmin");
+            txtPort.Text = ReadSetting(configFile, "Listen");
+            txtErrorLog.Text = ReadSetting(configFile, "ErrorLog");
+            txtLogLevel.Text = ReadSetting(configFile, "LogLevel");
+        }
+
+        private void btnCloseWindow_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void ResizeLogWindow(object sender, EventArgs e)
+        {
+            btnSave.Top = Height - 44;
+            button1.Top = Height - 44;
+            btnCloseWindow.Left = Width - btnCloseWindow.Width;
+            btnMinimize.Left = Width - btnCloseWindow.Width - btnMinimize.Width;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
