6cc72cddbfc1daa2b2f4c8134a9b419e10519d90
Dan <33081566+deobfuscate@users.noreply.github.com>
2021-11-21 23:40:15 -0800

    Migrate Apache Log window to new style

--- wampmon/frmLogs.Designer.cs
+++ wampmon/frmLogs.Designer.cs
@@ -29,80 +29,17 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmLogs));
-            this.pnlTitlebar = new System.Windows.Forms.Panel();
-            this.btnMinimize = new System.Windows.Forms.Button();
-            this.btnCloseWindow = new System.Windows.Forms.Button();
-            this.lblTitle = new System.Windows.Forms.Label();
             this.txtLogs = new System.Windows.Forms.TextBox();
             this.btnClearLogs = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
-            this.pnlTitlebar.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // pnlTitlebar
-            // 
-            this.pnlTitlebar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(10)))), ((int)(((byte)(10)))));
-            this.pnlTitlebar.Controls.Add(this.btnMinimize);
-            this.pnlTitlebar.Controls.Add(this.btnCloseWindow);
-            this.pnlTitlebar.Controls.Add(this.lblTitle);
-            this.pnlTitlebar.Dock = System.Windows.Forms.DockStyle.Top;
-            this.pnlTitlebar.Location = new System.Drawing.Point(0, 0);
-            this.pnlTitlebar.Name = "pnlTitlebar";
-            this.pnlTitlebar.Size = new System.Drawing.Size(562, 31);
-            this.pnlTitlebar.TabIndex = 2;
-            this.pnlTitlebar.DoubleClick += new System.EventHandler(this.TitleDoubleClick);
-            this.pnlTitlebar.MouseMove += new System.Windows.Forms.MouseEventHandler(this.TitleMouseMove);
-            // 
-            // btnMinimize
-            // 
-            this.btnMinimize.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(10)))), ((int)(((byte)(10)))));
-            this.btnMinimize.FlatAppearance.BorderSize = 0;
-            this.btnMinimize.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(75)))), ((int)(((byte)(75)))), ((int)(((byte)(75)))));
-            this.btnMinimize.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
-            this.btnMinimize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnMinimize.ForeColor = System.Drawing.Color.White;
-            this.btnMinimize.Location = new System.Drawing.Point(472, 0);
-            this.btnMinimize.Name = "btnMinimize";
-            this.btnMinimize.Size = new System.Drawing.Size(45, 31);
-            this.btnMinimize.TabIndex = 3;
-            this.btnMinimize.Text = "＿";
-            this.btnMinimize.UseVisualStyleBackColor = false;
-            this.btnMinimize.Click += new System.EventHandler(this.btnMinimize_Click);
-            // 
-            // btnCloseWindow
-            // 
-            this.btnCloseWindow.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(10)))), ((int)(((byte)(10)))));
-            this.btnCloseWindow.FlatAppearance.BorderSize = 0;
-            this.btnCloseWindow.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(75)))), ((int)(((byte)(75)))), ((int)(((byte)(75)))));
-            this.btnCloseWindow.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
-            this.btnCloseWindow.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnCloseWindow.ForeColor = System.Drawing.Color.White;
-            this.btnCloseWindow.Location = new System.Drawing.Point(517, 0);
-            this.btnCloseWindow.Name = "btnCloseWindow";
-            this.btnCloseWindow.Size = new System.Drawing.Size(45, 31);
-            this.btnCloseWindow.TabIndex = 2;
-            this.btnCloseWindow.Text = "✕";
-            this.btnCloseWindow.UseVisualStyleBackColor = false;
-            this.btnCloseWindow.Click += new System.EventHandler(this.btnClose_Click);
-            // 
-            // lblTitle
-            // 
-            this.lblTitle.AutoSize = true;
-            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTitle.ForeColor = System.Drawing.Color.White;
-            this.lblTitle.Location = new System.Drawing.Point(8, 6);
-            this.lblTitle.Name = "lblTitle";
-            this.lblTitle.Size = new System.Drawing.Size(219, 20);
-            this.lblTitle.TabIndex = 0;
-            this.lblTitle.Text = "WAMP Monitor - Access Logs";
-            this.lblTitle.MouseMove += new System.Windows.Forms.MouseEventHandler(this.TitleLabelMouseMove);
             // 
             // txtLogs
             // 
             this.txtLogs.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
             this.txtLogs.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.txtLogs.ForeColor = System.Drawing.Color.White;
-            this.txtLogs.Location = new System.Drawing.Point(12, 37);
+            this.txtLogs.Location = new System.Drawing.Point(12, 12);
             this.txtLogs.Multiline = true;
             this.txtLogs.Name = "txtLogs";
             this.txtLogs.ReadOnly = true;
@@ -117,7 +54,7 @@
             this.btnClearLogs.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(75)))), ((int)(((byte)(75)))), ((int)(((byte)(75)))));
             this.btnClearLogs.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnClearLogs.ForeColor = System.Drawing.Color.White;
-            this.btnClearLogs.Location = new System.Drawing.Point(14, 321);
+            this.btnClearLogs.Location = new System.Drawing.Point(14, 296);
             this.btnClearLogs.Name = "btnClearLogs";
             this.btnClearLogs.Size = new System.Drawing.Size(94, 36);
             this.btnClearLogs.TabIndex = 4;
@@ -132,7 +69,7 @@
             this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(75)))), ((int)(((byte)(75)))), ((int)(((byte)(75)))));
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.ForeColor = System.Drawing.Color.White;
-            this.button1.Location = new System.Drawing.Point(114, 321);
+            this.button1.Location = new System.Drawing.Point(114, 296);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(94, 36);
             this.button1.TabIndex = 4;
@@ -145,31 +82,22 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(18)))), ((int)(((byte)(18)))));
-            this.ClientSize = new System.Drawing.Size(562, 365);
+            this.ClientSize = new System.Drawing.Size(562, 345);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.btnClearLogs);
             this.Controls.Add(this.txtLogs);
-            this.Controls.Add(this.pnlTitlebar);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MinimumSize = new System.Drawing.Size(300, 250);
             this.Name = "frmLogs";
             this.Text = "WAMP Monitor - Access logs";
             this.Load += new System.EventHandler(this.frmLogs_Load);
             this.Resize += new System.EventHandler(this.ResizeLogWindow);
-            this.pnlTitlebar.ResumeLayout(false);
-            this.pnlTitlebar.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Panel pnlTitlebar;
-        private System.Windows.Forms.Button btnMinimize;
-        private System.Windows.Forms.Button btnCloseWindow;
-        private System.Windows.Forms.Label lblTitle;
         private System.Windows.Forms.TextBox txtLogs;
         private System.Windows.Forms.Button btnClearLogs;
         private System.Windows.Forms.Button button1;
