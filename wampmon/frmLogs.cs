39d80bd23ed81b6e4a7e06f0f7f02089c9393df4
Dan <33081566+deobfuscate@users.noreply.github.com>
2021-12-06 23:56:33 -0800

    Remove unused code

--- wampmon/frmLogs.cs
+++ wampmon/frmLogs.cs
@@ -8,10 +8,6 @@ namespace wampmon
 {
     public partial class frmLogs : Form
     {
-        private const int WM_NCLBUTTONDOWN = 0xA1;
-        private const int HT_CAPTION = 0x2;
-        private const int GRIP_SIZE = 14;
-
         [DllImport("user32.dll")]
         private static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
         [DllImport("user32.dll")]
@@ -37,58 +33,6 @@ namespace wampmon
             DoubleBuffered = true;
             SetStyle(ControlStyles.ResizeRedraw, true);
             apacheLogFile = logFile;
-        }
-
-        private void TitleDoubleClick(object sender, EventArgs e)
-        {
-            if (WindowState == FormWindowState.Normal) WindowState = FormWindowState.Maximized;
-            else WindowState = FormWindowState.Normal;
-        }
-
-        private void TitleMouseMove(object sender, MouseEventArgs e)
-        {
-            if (e.Button == MouseButtons.Left)
-            {
-                ReleaseCapture();
-                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
-            }
-        }
-
-        protected override void OnPaint(PaintEventArgs e)
-        {
-            Rectangle rc = new Rectangle(ClientSize.Width - GRIP_SIZE, ClientSize.Height - GRIP_SIZE, GRIP_SIZE, GRIP_SIZE);
-            ControlPaint.DrawSizeGrip(e.Graphics, BackColor, rc);
-        }
-
-        protected override void WndProc(ref Message m)
-        {
-            if (m.Msg == 0x84)
-            {  // Trap WM_NCHITTEST
-                Point pos = new Point(m.LParam.ToInt32());
-                pos = PointToClient(pos);
-
-                if (pos.X >= ClientSize.Width - GRIP_SIZE && pos.Y >= ClientSize.Height - GRIP_SIZE)
-                {
-                    m.Result = (IntPtr)17; // HTBOTTOMRIGHT
-                    return;
-                }
-            }
-            base.WndProc(ref m);
-        }
-
-        private void TitleLabelMouseMove(object sender, MouseEventArgs e)
-        {
-            TitleMouseMove(sender, e);
-        }
-
-        private void btnMinimize_Click(object sender, EventArgs e)
-        {
-            WindowState = FormWindowState.Minimized;
-        }
-
-        private void btnClose_Click(object sender, EventArgs e)
-        {
-            Close();
         }
 
         private void frmLogs_Load(object sender, EventArgs e)
--- wampmon/frmPHPSettings.cs
+++ wampmon/frmPHPSettings.cs
@@ -6,7 +6,6 @@ namespace wampmon
 {
     public partial class frmPHPSettings : Form
     {
-
         [DllImport("user32.dll")]
         private static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
         [DllImport("user32.dll")]
@@ -32,11 +31,6 @@ namespace wampmon
             DoubleBuffered = true;
             //SetStyle(ControlStyles.ResizeRedraw, true);
             this.configFile = configFile;
-        }
-
-        private string ReadSetting(string filename, string setting)
-        {
-            return null;
         }
 
         private void btnMinimize_Click(object sender, EventArgs e)
