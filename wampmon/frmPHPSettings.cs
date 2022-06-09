d9c3512e96a3baa3f8a23958f981fecc19c285fb
Dan <33081566+deobfuscate@users.noreply.github.com>
2022-06-08 22:54:42 -0700

    Remove unused DLL calls

--- wampmon/frmPHPSettings.cs
+++ wampmon/frmPHPSettings.cs
@@ -6,11 +6,6 @@ namespace wampmon
 {
     public partial class frmPHPSettings : Form
     {
-        [DllImport("user32.dll")]
-        private static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
-        [DllImport("user32.dll")]
-        private static extern bool ReleaseCapture();
-
         private string configFile;
         
         public frmPHPSettings(string configFile)
@@ -28,7 +23,7 @@ namespace wampmon
 
         private void frmLogs_Load(object sender, EventArgs e)
         {
-            comboBox2.SelectedIndex= 0;
+            comboBox2.SelectedIndex = 0;
         }
 
         private void btnSave_Click(object sender, EventArgs e)
