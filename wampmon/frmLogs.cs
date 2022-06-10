b1df6cf049dff02a3ac3845978ef313803a16e6e
Dan <33081566+deobfuscate@users.noreply.github.com>
2022-06-09 18:05:06 -0700

    Remove unused DLL calls

--- wampmon/frmLogs.cs
+++ wampmon/frmLogs.cs
@@ -8,11 +8,6 @@ namespace wampmon
 {
     public partial class frmLogs : Form
     {
-        [DllImport("user32.dll")]
-        private static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
-        [DllImport("user32.dll")]
-        private static extern bool ReleaseCapture();
-
         private readonly string apacheLogFile;
 
         public frmLogs(string logFile)
